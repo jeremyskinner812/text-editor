27de7688d59d273b302a7f3b3abb20eea2510f63
Jeremy <jdskinne@gmail.com>
2023-01-20 20:37:03 -0600

    added webpack pwa manifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,7 +26,24 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-      
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'TextEditor',
+        short_name: 'TextEditor',
+        description: 'PWA text editor',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
